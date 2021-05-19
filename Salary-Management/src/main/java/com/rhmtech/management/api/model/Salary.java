8ea18dc6d5e2e3e38708490d206819386037eabb
rakib lanovo office <rfinfo247@gmail.com>
2021-05-20 00:50:31 +0600

    ok

---
+++ Salary-Management/src/main/java/com/rhmtech/management/api/model/Salary.java
@@ -0,0 +1,21 @@
+package com.rhmtech.management.api.model;
+
+import javax.persistence.Column;
+
+public class Salary {
+	
+	private long emp_id;
+	private String emp_name;
+	private long salary_grade;
+	private String emp_address;
+	private String emp_mobile;
+	private String accnumber;
+	private String acc_name;
+	private String acc_type;
+	private float curr_bal;
+	private String bank_name;
+	private String bank_branch;
+	private String gradename;
+	private float basicAmount;
+
+}
