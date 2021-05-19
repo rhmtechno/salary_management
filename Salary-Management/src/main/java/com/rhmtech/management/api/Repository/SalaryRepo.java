1948b77dd685e808651de225dae0ba61ff996777
rakib lanovo office <rfinfo247@gmail.com>
2021-05-20 02:58:20 +0600

    query modify

--- Salary-Management/src/main/java/com/rhmtech/management/api/Repository/SalaryRepo.java
+++ Salary-Management/src/main/java/com/rhmtech/management/api/Repository/SalaryRepo.java
@@ -7,7 +7,7 @@ import com.rhmtech.management.api.model.Salary;
 
 public interface SalaryRepo extends JpaRepository<Salary, Long> {
 
-	@Query(value = "select 1 as s_id, emp.emp_id,emp.emp_address,emp.salary_grade,emp.emp_mobile,emp.emp_name,ebt.acc_name,ebt.acc_type,\r\n"
+	@Query(value = "select ebt.accid as s_id,emp.emp_id,emp.emp_address,emp.salary_grade,emp.emp_mobile,emp.emp_name,ebt.acc_name,ebt.acc_type,\r\n"
 			+ "ebt.accnumber,ebt.bank_branch,ebt.bank_name,ebt.curr_bal,gt.basic_amount,gt.gradename,gt.house_rent,gt.medical_allowance,\r\n"
 			+ "gt.basic_amount+(gt.basic_amount*(gt.house_rent)/100)+(gt.basic_amount*(gt.medical_allowance)/100) as total_salary\r\n"
 			+ "from employee_tbl emp\r\n"
@@ -18,7 +18,7 @@ public interface SalaryRepo extends JpaRepository<Salary, Long> {
 	List<Salary> getAllSalary();
 	
 	
-	@Query(value = "select  1 as s_id, emp.emp_id,emp.emp_address,emp.salary_grade,emp.emp_mobile,emp.emp_name,ebt.acc_name,ebt.acc_type,\r\n"
+	@Query(value = "select  ebt.accid as s_id, emp.emp_id,emp.emp_address,emp.salary_grade,emp.emp_mobile,emp.emp_name,ebt.acc_name,ebt.acc_type,\r\n"
 			+ "ebt.accnumber,ebt.bank_branch,ebt.bank_name,ebt.curr_bal,gt.basic_amount,gt.gradename,gt.house_rent,gt.medical_allowance,\r\n"
 			+ "gt.basic_amount+(gt.basic_amount*(gt.house_rent)/100)+(gt.basic_amount*(gt.medical_allowance)/100) as total_salary\r\n"
 			+ "from employee_tbl emp\r\n"
