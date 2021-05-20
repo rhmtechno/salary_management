508c55c9aea168f6ac2ff9a86f072ea1c5802f36
rakib lanovo office <rfinfo247@gmail.com>
2021-05-20 19:02:37 +0600

    client add

---
+++ client/Office-Management-client/src/main/java/com/rhmtech/ui/api/OfficeManagementClientApplication.java
@@ -0,0 +1,13 @@
+package com.rhmtech.ui.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficeManagementClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OfficeManagementClientApplication.class, args);
+	}
+
+}
---
+++ client/Office-Management-client/src/test/java/com/rhmtech/ui/api/OfficeManagementClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rhmtech.ui.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeManagementClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
