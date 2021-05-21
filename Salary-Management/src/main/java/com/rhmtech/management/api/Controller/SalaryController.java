3e8903c8289a8e77ad6cfcbd4135a1f4ad1842e0
rakib lanovo office <rfinfo247@gmail.com>
2021-05-21 09:19:55 +0600

    p=ok

--- Salary-Management/src/main/java/com/rhmtech/management/api/Controller/SalaryController.java
+++ Salary-Management/src/main/java/com/rhmtech/management/api/Controller/SalaryController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,6 +15,7 @@ import com.rhmtech.management.api.service.SalaryService;
 
 @RestController
 @RequestMapping("/salary")
+@CrossOrigin(origins = "http://localhost:4200")
 public class SalaryController {
 	@Autowired
 	private SalaryService service;
